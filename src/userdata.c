f2ec4d19b258d98961df9cefdae9321560b4972f
Nauxitt <gillchirst.pitts@gmail.com>
2019-12-10 14:32:12 -0800

    Hunter saving/loading

--- src/userdata.c
+++ src/userdata.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <time.h>
 #include "hunter.h"
+#include <string.h>
 
 
 char __data_dir[128];
@@ -50,9 +51,84 @@ char * dataPath(char * dest, char * sub_path){
 	return dest;
 }
 
-int usermain(){
-	// https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+void hunterSaveAt(Hunter * hunter, char * fpath){
+	/*
+		Saves hunter data at a specified path.
+	*/
+
+	char buffer[256];
+	encodeHunter(hunter, (char*) &buffer);
 	
+	// Get data length
+	char * seek = (char*) &buffer;
+	
+	// Seek NULL byte, ending the header
+	while(*seek++ != NULL);
+	
+	// Decode length from characters
+	uint32_t length;
+	length  = *seek++ << 24;
+	length |= *seek++ << 16;
+	length |= *seek++ <<  8;
+	length |= *seek++;
+	
+	// Save hunter data
+	FILE * fd = fopen(fpath, "wb");
+	// TODO: error check
+	
+	fwrite(&buffer, length, 1, fd);
+	fclose(fd);
+}
+
+void hunterSave(Hunter * hunter){
+	/*
+	   Saves hunter in $DATA/hunters/$HUNTER_NAME.hunter
+	*/
+
+	char fpath[256];
+
+	// Get hunter filepath
+	sprintf(
+			(char*) &fpath,
+			"%s/hunters/%s.hunter",
+			dataDir(),
+			(char*) hunter->name
+		);
+
+	hunterSaveAt(hunter, (char*) &fpath);
+}
+
+int userdataMainSave() {
+	Hunter hunters[] = {
+		{	.name = "DANIEL",
+		.base_stats={.atk = 2, .mov = 3, .def = 6, .max_hp=10},
+		.credits = 10528,
+		.type = "hunter"
+		},
+		{	.name = "DAVE",
+		.base_stats = {.mov = 3, .atk = 4, .def = 4, .max_hp=10},
+		.credits = 1,
+		.type = "hunter"
+		},
+		{	.name = "STAN",
+		.base_stats = {.atk = 2, .mov = 1, .def = 8, .max_hp=10},
+		.credits = 0xFFFFFFFF,
+		.type = "hunter"
+		},
+		{	.name = "TIM",
+		.base_stats = {.atk = 9, .mov = 1, .def = 1, .max_hp=10},
+		.credits = 12,
+		.type = "hunter"
+		}
+	};
+
+	for(int i=0; i < 4; i++){
+		hunterSave(&hunters[i]);
+	}
+}
+
+int usermain(){
+	userdataMainSave();
 	char buffer[256];
 
 	// Linked list struct type for Hunter reading
@@ -125,8 +201,15 @@ int usermain(){
 
 	struct HunterFileLinkedList * iter = hunters;
 	for(; iter; iter = iter->next){
-		printf("%s\n", (char*) &iter->fpath);
-		printf("Modified time: %s\n", ctime(&iter->attr.st_mtime));
+		// Load hunter file contents
+		FILE * fd = fopen((char*) &iter->fpath, "rb");
+		fread((char*) &buffer, sizeof(buffer), 1, fd);
+		fclose(fd);
+
+		// Decode hunter data and print
+		decodeHunter(&iter->hunter, (char*) &buffer);
+		printHunter(&iter->hunter);
+		printf("\n");
 	}
 
 	return 0;
--- src/userdata.h
+++ src/userdata.h
@@ -1,10 +1,17 @@
 #ifndef __userdata_h
 #define __userdata_h
 
+#include "hunter.h"
+
 extern char __data_dir[128];
 
 char * dataDir();
 char * dataPath(char * dest, char * sub_path);
+
+void hunterSaveAt(Hunter * hunter, char * fpath);
+void hunterSave(Hunter * hunter);
+
+int userdataMainSave();
 int usermain();
 
 #endif
